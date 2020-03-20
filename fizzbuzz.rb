4f63fb6bc88027912d2d8a21630a7f1be7059f46
Evan Burton <evan@heyevan.com>
2020-03-20 03:58:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,3 +3,8 @@ def fizzbuzz(int) # Don't forget! This file needs to be 'required' in its spec f
     "Fizz" # See README.md for instructions on how to do this
   end
 end
+
+def fizzbuzz(int)
+  if int % 5 == 0
+  end
+end
